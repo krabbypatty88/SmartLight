5961d67d769fa2e8c40927621c943337a3f0bf39
Patrick Setterlund <patricksetterlund10@gmail.com>
2025-06-24 21:38:30 +0900

    Finished off function for tetris lighting stream. Testing still necessary

--- backend-pi/patternModes/tetris.py
+++ backend-pi/patternModes/tetris.py
@@ -4,9 +4,29 @@ import time
 # Helpers --------
 
 # Colour pattern that represents that the tetris round has been completed
-def finishing_sequence(neo, delay=0.05, stop_event=None):
+def finishing_sequence(neo, color, delay=0.05, stop_event=None):
   if stop_event and stop_event.is_set():
     return
+  neo.fill_strip(color)
+  neo.update_strip()
+  time.sleep(delay)
+  
+  # Gradually empty the grid before restarting the tetris pattern
+  pos = neo.num_leds
+  while pos >= 0:
+    neo.set_led_color(pos, (0, 0, 0))
+    neo.update_strip()
+    pos-=1
+    time.sleep(delay)
+  
+# Compute block length for the next iteration of the tetris pattern
+def compute_block_length(space_remaining):
+  if 0 < space_remaining < 4: 
+    return random.randint(1, space_remaining)
+  elif space_remaining > 0: 
+    return random.randint(1, 4)
+  elif space_remaining == 0:
+    return 0
   
 # Tetris ----------
 
@@ -28,15 +48,16 @@ def Tetris(neo, color, delay=0.05, stop_event=None):
 
     # While the strip is not filled, generate and propagate blocks
     while filled == False:
-
+      if stop_event and stop_event.is_set():
+        return
       space_remaining = neo.num_leds - strip_end
-      if 0 < space_remaining < 4: 
-        block_length = random.randint(1, space_remaining)
-      elif space_remaining > 0: 
-        block_length = random.randint(1, 4)
-      elif space_remaining == 0:
+
+      block_length = compute_block_length(space_remaining)
+      
+      if (block_length == 0):
         filled = True
-        finishing_sequence(neo, stop_event=stop_event)
+        finishing_sequence(neo, color, stop_event=stop_event)
+        break
 
       offset = 0 # keeps track of the current position of the block moving down the strip
       # reset LEDs which are currently not filled
@@ -53,16 +74,18 @@ def Tetris(neo, color, delay=0.05, stop_event=None):
 
       # Propagation of the block down the strip
       while moving == True:
+        if stop_event and stop_event.is_set():
+          return
         offset += 1
         for i in range(1, strip_end):
           neo.set_led_color(i, (0, 0, 0))
         for i in range(1, block_length):
           neo.set_led_color(i + offset, color)
-          time.sleep(delay)
           # Check if block has reached the end of the region that is not filled
           if (block_length + offset + 1) == strip_end:
             moving = False
         neo.update_strip()
+        time.sleep(delay)
 
       # Block reaches the end of the strip
       strip_end = strip_end - block_length
